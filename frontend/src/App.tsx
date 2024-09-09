e54be4d86881bf036387039a2ba9af6b1c10be3e
djkenneth <dj.kenneth.pineda@gmail.com>
2024-09-09 15:38:36 +0800

    Added react query and devtool

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,31 @@
 // import useFetch from './hooks/useFetch';
 
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./Layout";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Layout from "@/Layout";
+import { Home } from "@/pages/Home";
+import { Products } from "@/pages/Products";
+
+// Create a client
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <div className="app">
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+          </Routes>
+        </Layout>
+        {/* The rest of your application */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
