39885d39359bfa0395b1205c70a5df4bfe9e9923
djkenneth <dj.kenneth.pineda@gmail.com>
2024-09-09 15:42:25 +0800

    Create a function for convert price to PHP

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatNumberToCurrency(price: number) {
+  const PHPeso = new Intl.NumberFormat('fil-PH', {
+    style: 'currency',
+    currency: 'PHP'
+  });
+
+  return PHPeso.format(price)
+}
