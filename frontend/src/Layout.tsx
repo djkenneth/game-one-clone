ea2e146dc23e75494a04287cc586032f6a9c9670
djkenneth <dj.kenneth.pineda@gmail.com>
2024-09-09 15:37:18 +0800

    Added layout for header and footer

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Header } from './components/global/Header'
-import Footer from './components/global/footer'
+import { Header } from '@/components/global/Header'
+import { Footer } from '@/components/global/Footer'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
--- frontend/src/components/global/Header.tsx
+++ frontend/src/components/global/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
 
     return (
         <header className="bg-white">
-            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 2xl:max-w-[90%]">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
@@ -79,7 +79,7 @@ export const Header = () => {
               </PopoverGroup> */}
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Log in <span aria-hidden="true">&rarr;</span>
+                        Log in
                     </a>
                 </div>
             </nav>
